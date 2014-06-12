8793812ab4489891126c609807a62456cde50023
Evan Hensleigh <futuraprime@gmail.com>
2014-06-11 20:40:25 -0400

    Getting going...

--- js/offside.js
+++ js/offside.js
@@ -1,3 +1,28 @@
 (function() {
-  
+  function recalculatePositions() {
+
+  }
+
+  function Team() {
+    this.players = [];
+  }
+  Team.prototype.addPlayer = function() {
+
+  };
+
+  function Player(field) {
+    this.representation = field.circle(0,0,this.radius);
+    this.representation.drag(this.onMove.bind(this));
+  }
+  Player.prototype.radius = 10;
+  Player.prototype.onMove = function(evt) {
+    console.log('moving!', arguments);
+    recalculatePositions();
+  };
+
+
+  var s = Snap('#interactive');
+
+  var p = new Player(s);
+
 }).call(this);
