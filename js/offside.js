62d38bd09ada17436a325615b902d3b0f89352c5
Evan Hensleigh <futuraprime@gmail.com>
2014-06-11 21:44:08 -0400

    And now we can draw a pitch.

--- js/offside.js
+++ js/offside.js
@@ -1,10 +1,115 @@
 (function() {
+  function Pitch(field, scale) {
+    scale = scale || 1;
+    function scaleValue(v) {
+      v = v || 0;
+      return v * scale;
+    }
+
+    // these are the dimensions of the Maracana
+    // all lengths in feet
+    var l = this.length = scaleValue(344);
+    var w = this.width = scaleValue(223);
+
+    var g = this.group = field.group();
+    g.attr({
+      transform : 'translate(50,50)'
+    });
+
+    var padding = 20;
+    var lineAttrs = {
+      fill : 'none',
+      stroke : '#fff',
+      'stroke-width' : 1.5,
+      'stroke-linecap' : 'square'
+    };
+    var spotAttrs = {
+      fill : '#fff',
+      stroke : 'none',
+      r : 4
+    };
+    var pitchColor = '#337331';
+
+    this.background = field.rect(-padding,-padding,l+padding*2,w+padding*2);
+    this.background.attr({
+      fill : pitchColor
+    });
+    g.add(this.background);
+
+    this.leftGoalLine = field.line(0,0,0,w).attr(lineAttrs);
+    this.rightGoalLine = field.line(l,0,l,w).attr(lineAttrs);
+    this.topLine = field.line(0,0,l,0).attr(lineAttrs);
+    this.bottomLine = field.line(0,w,l,w).attr(lineAttrs);
+    this.centerLine = field.line(l/2,0,l/2,w).attr(lineAttrs);
+    this.centerCircle = field.circle(l/2,w/2,30).attr(lineAttrs);
+    this.centerSpot = field.circle(l/2,w/2,5).attr(spotAttrs);
+
+    g.add(
+      this.leftGoalLine,
+      this.rightGoalLine,
+      this.topLine,
+      this.bottomLine,
+      this.centerLine,
+      this.centerCircle,
+      this.centerSpot
+    );
+
+    function positionBoxToSide(right, length) {
+      if(length < 0) {
+        return right ? l : scaleValue(length);
+      }
+      return right ? l - scaleValue(length) : 0;
+    }
+    function centerCircleToSide(right, dL) {
+      return right ? l - scaleValue(dL) : scaleValue(dL);
+    }
+    function centerOnWidth(width) {
+      return (w - scaleValue(width)) / 2;
+    }
+    function sideBoxCircle(right,cx,r) {
+      return field.circle(
+        centerCircleToSide(right, cx),
+        centerOnWidth(),
+        scaleValue(r)
+      );
+    }
+    function sideBoxRect(right,length,width) {
+      return field.rect(
+        positionBoxToSide(right, length),
+        centerOnWidth(width),
+        scaleValue(Math.abs(length)),
+        scaleValue(width)
+      );
+    }
+
+    function drawSideBox(right) {
+      var penaltyCircle = sideBoxCircle(right, 36, 30).attr(lineAttrs);
+      // filling this one to hide most of the penalty circle
+      var penaltyBox = sideBoxRect(right, 54, 132).attr(lineAttrs).attr({fill:pitchColor});
+      var penaltySpot = sideBoxCircle(right, 36,2).attr(spotAttrs);
+      var goalBox = sideBoxRect(right, 18, 60).attr(lineAttrs);
+      var goal = sideBoxRect(right,-8,24).attr(lineAttrs);
+
+      g.add(
+        penaltyCircle,
+        penaltyBox,
+        penaltySpot,
+        goalBox,
+        goal
+      );
+    }
+
+    drawSideBox(false);
+    drawSideBox(true);
+  }
+
   function recalculatePositions() {
 
   }
 
-  function Team() {
+  function Team(side, color) {
     this.players = [];
+    this.color = color;
   }
   Team.prototype.addPlayer = function() {
 
@@ -20,8 +125,9 @@
     recalculatePositions();
   };
 
-
   var s = Snap('#interactive');
+
+  var pitch = new Pitch(s, 1.5);
 
   var p = new Player(s);
 
